46ce2543e428401371adc9b4c6574304a1406146
Matt Rushton <mrushton@blogcastr.com>
2011-02-21 19:08:56 -0500

    First Commit
    Initial commit of default Xcode Universal App with Core Data.

---
+++ iPad/AppDelegate_iPad.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate_iPad.h
+//  Blogcastr
+//
+//  Created by Matthew Rushton on 2/21/11.
+//  Copyright 2011 Blogcastr. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate_Shared.h"
+
+@interface AppDelegate_iPad : AppDelegate_Shared {
+}
+
+
+@end
+
---
+++ iPhone/AppDelegate_iPhone.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate_iPhone.h
+//  Blogcastr
+//
+//  Created by Matthew Rushton on 2/21/11.
+//  Copyright 2011 Blogcastr. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate_Shared.h"
+
+@interface AppDelegate_iPhone : AppDelegate_Shared {
+}
+
+
+@end
+
